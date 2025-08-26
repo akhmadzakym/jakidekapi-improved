d9e1c3b4a9d9bb99f16fe35205b4d182e3f24a40
akhmadzakym <akhmadzaky.m@gmail.com>
2025-08-27 01:49:11 +0700

    Adding embeds and tweaking them a little bit  Changes to be committed: 	modified:   src/pages/Disco.tsx 	modified:   src/pages/Video.tsx

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -8,7 +8,12 @@ const Video = () => {
 
     return (
         <div className="ml-10 mr-10 flex flex-col flex-wrap gap-3">
-            <h1>NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. NOPE. </h1>
+            <center>
+            <iframe width="996" height="560" src="https://www.youtube.com/embed/CWHXb7S4AHw" title="Man" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </center>
+            <center>
+            <iframe width="996" height="560" src="https://www.youtube.com/embed/DugDkLWyfqU" title="broktin - elektrionk" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </center>
         </div>
 
     );
